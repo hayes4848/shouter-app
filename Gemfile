4673884e1794b45f2b0acaf71e77e5cdd46439bb
Five Star <fivestardev@fivestardev-VirtualBox.(none)>
2014-10-27 16:07:56 -0600

    initial commit for shouter app

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.17'
+gem 'sqlite3'
+gem 'strong_parameters', '0.2.0'
+gem 'monban', '0.0.6'
+gem 'jquery-rails'
+gem 'better_errors'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '4bdf7c43275d4174d4c3a41b3a5f5e86f80c17b08d1c21e82388b4fb54ed0a5f0eea5cb61c39ea686a835c072d5a9442e1e43a9f6bd582001644ba41601d8dd7'
